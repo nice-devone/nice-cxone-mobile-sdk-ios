202006b93d56c3fe962cd6c036bf035e9c1b0933
Lukáš Šanda <sanda.lucas@gmail.com>
2025-02-12 14:18:34 +0100

    [chore] add generated file

---
+++ Sources/Resources/Generated/CXoneChatSDKModule.swift
@@ -0,0 +1,23 @@
+//
+// Copyright (c) 2021-2024. NICE Ltd. All rights reserved.
+//
+// Licensed under the NICE License;
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    https://github.com/nice-devone/nice-cxone-mobile-sdk-ios/blob/main/LICENSE
+//
+// TO THE EXTENT PERMITTED BY APPLICABLE LAW, THE CXONE MOBILE SDK IS PROVIDED ON
+// AN “AS IS” BASIS. NICE HEREBY DISCLAIMS ALL WARRANTIES AND CONDITIONS, EXPRESS
+// OR IMPLIED, INCLUDING (WITHOUT LIMITATION) WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, NON-INFRINGEMENT, AND TITLE.
+//
+//  ⚠️ Generated source file -> do NOT edit ⚠️
+
+import Foundation
+
+/// A generated structure that holds information about the module version.
+public struct CXoneChatSDKModule {
+    /// The current marketing version of the CXoneChatSDKModule
+    public static let version: String = "2.3.0"
+}
